6a8bf35e7ff85b20ea730c1db9fb18303bad8050
kura-okubo <kurama_okubo@fas.harvard.edu>
2019-07-08 18:06:54 -0400

    update using varm

--- src/scratch.jl
+++ src/scratch.jl
@@ -5,3 +5,24 @@ a = Dict( "finame"  => "test",
 for key in keys(a)
     println(@sprintf("%10s = %10s", key, string(a["$key"])))
 end
+
+
+a = [1,2,3]
+b = mapreduce(identity, +, a)
+b = mapreduce(x->x^2, +, a)
+
+c = zeros(5)
+fill!(c, 3)
+
+
+m = 10
+v = collect(1:100)
+TN=10
+function _f_kurt1(v, m)
+    return (v - m)^2
+end
+
+z2 = map(x -> _f_kurt1(x, m), v[1:TN])
+
+a = [1,2,3]
+map(x -> x^2, a)
